5b9ba5c9f7ea2e6b2d1fba05ada5b4c60f282f06
yoshi <yoshitakaOkada0214@gmail.com>
2021-11-21 16:55:20 +0900

    testを整えた

--- greeter.ts
+++ greeter.ts
@@ -2,6 +2,7 @@ export class Greeter {
     constructor(private name: string) {}
 
     getMessage(optionMessage: string) {
+        // 変数を埋め込む書き方をテンプレートリテラルという（バッククオートで囲む）
         return `Hello ${this.name} ${optionMessage}`;
     }
 }
